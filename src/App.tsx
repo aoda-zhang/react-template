77ac475060ac0527eda67192d57a70d205cbd1ef
aoda <aoda2826@qq.com>
2022-04-19 23:04:36 +0800

    feat:添加nodealize.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Explain from './common/components/Explain'
+// 引入浏览器默认样式
+import 'normalize.css'
 
 const App: React.FC = () => {
-  return (
-    <>
-      <Explain></Explain>
-    </>
-  )
+  return <Explain></Explain>
 }
 
 export default App
